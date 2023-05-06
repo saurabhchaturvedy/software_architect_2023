4f535f85020773fa108651488608a0f9071b8931
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-05-06 17:27:59 +0530

    dsa

--- src/main/java/Fundamentals/Queue/Queue.java
+++ src/main/java/Fundamentals/Queue/Queue.java
@@ -1,6 +1,7 @@
 package Fundamentals.Queue;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Queue {
 
@@ -38,7 +39,68 @@ public class Queue {
     }
 
 
+    public void enqueue(int data) {
+        ListNode temp = new ListNode(data);
+
+        if (isEmpty()) {
+            front = temp;
+        } else {
+            rear.next = temp;
+        }
+
+        rear = temp;
+        length++;
+    }
+
+
+    public void print() {
+
+        if (isEmpty()) {
+            return;
+        }
+
+        ListNode current = front;
+
+        while (current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
+    }
+
+
+    public int dequeue() {
+
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is already empty !!");
+        }
+
+        int result = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+
+        length--;
+        return result;
+    }
+
+
     public static void main(String[] args) {
 
+
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(15);
+        queue.enqueue(20);
+        queue.enqueue(25);
+
+        queue.print();
+
+
+        queue.dequeue();
+
+        queue.print();
     }
 }
