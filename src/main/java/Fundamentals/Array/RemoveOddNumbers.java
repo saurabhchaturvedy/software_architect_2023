3ddfe2341c5e004ff6b32fcd277e66e6a31866b3
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-05-06 14:45:15 +0530

    dsa

--- src/main/java/Fundamentals/Array/RemoveOddNumbers.java
+++ src/main/java/Fundamentals/Array/RemoveOddNumbers.java
@@ -27,15 +27,33 @@ public class RemoveOddNumbers {
     }
 
 
+    public int[] resize(int[] arr, int capacity) {
+        int[] temp = new int[capacity];
+
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        arr = temp;
+        return arr;
+    }
+
+
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5, 6};
 
         RemoveOddNumbers removeOddNumbers = new RemoveOddNumbers();
-        int[] ints = removeOddNumbers.removeOddNumbers(arr);
+        //   int[] ints = removeOddNumbers.removeOddNumbers(arr);
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(ints[i]);
+            //        System.out.println(ints[i]);
         }
+
+        System.out.println("size of the array is : " + arr.length);
+
+        arr = removeOddNumbers.resize(arr, 10);
+
+        System.out.println("size of the array is : " + arr.length);
     }
 }
