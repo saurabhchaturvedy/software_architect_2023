a0a8256d8ec005229cecb146ad580c7a3a08609f
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-05-06 12:59:51 +0530

    dsa

--- src/main/java/Last_Minute_Brush_Up_Problems/SpiralMatrix.java
+++ src/main/java/Last_Minute_Brush_Up_Problems/SpiralMatrix.java
@@ -3,47 +3,42 @@ package Last_Minute_Brush_Up_Problems;
 public class SpiralMatrix {
 
 
-    public static void spiralMatrix(int[][]matrix)
-    {
+    public static void spiralMatrix(int[][] matrix) {
         int top = 0;
-        int bottom = matrix.length-1;
+        int bottom = matrix.length - 1;
         int left = 0;
-        int right = matrix[0].length-1;
-        int direction=0;
+        int right = matrix[0].length - 1;
+        int direction = 0;
 
-        while (top<=bottom && left<=right) {
+        while (top <= bottom && left <= right) {
 
             if (direction == 0) {
                 for (int i = left; i <= right; i++) {
-                    System.out.print(matrix[top][i]+" ");
+                    System.out.print(matrix[top][i] + " ");
                 }
                 top++;
-            } else if (direction==1) {
+            } else if (direction == 1) {
 
-                for(int i=top; i<=bottom; i++)
-                {
-                    System.out.println(matrix[i][right]+" ");
+                for (int i = top; i <= bottom; i++) {
+                    System.out.println(matrix[i][right] + " ");
                 }
-right--;
-            } else if (direction==2) {
+                right--;
+            } else if (direction == 2) {
 
-                for(int i=right; i>=left; i--)
-                {
-                    System.out.print(matrix[bottom][i]+" ");
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
                 }
                 bottom--;
-            }
-            else {
+            } else {
 
-                for(int i=bottom; i>=top; i--)
-                {
-                    System.out.print(matrix[i][left]+" ");
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
                 }
 
                 left++;
             }
 
-            direction=(direction+1)%4;
+            direction = (direction + 1) % 4;
         }
 
 
@@ -52,11 +47,7 @@ right--;
 
     public static void main(String[] args) {
 
-        int[][]arr = {
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12}
-        };
+        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
         spiralMatrix(arr);
     }
