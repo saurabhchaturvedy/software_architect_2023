d8cbbc9a22afd0755a8ae38e8f8bfc761f7010ae
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-05-27 23:44:58 +0530

    dsa

---
+++ src/main/java/Fundamentals2/Intervals/InsertIntervals.java
@@ -0,0 +1,55 @@
+package Fundamentals2.Intervals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertIntervals {
+
+
+    public int[][] insertIntervals(int[][] intervals, int[] newInterval) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        List<int[]> finalIntervals = new ArrayList<>();
+        int index = 0;
+        int lowVal = newInterval[0];
+        int highVal = newInterval[1];
+        while (index < intervals.length && intervals[index][1] < newInterval[0]) {
+            finalIntervals.add(intervals[index++]);
+        }
+
+        while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
+
+            lowVal = Math.min(lowVal, intervals[index][0]);
+            highVal = Math.max(highVal, intervals[index][1]);
+            index++;
+        }
+
+        finalIntervals.add(new int[]{lowVal, highVal});
+
+        while (index < intervals.length) {
+
+            finalIntervals.add(intervals[index++]);
+        }
+
+        return finalIntervals.toArray(new int[finalIntervals.size()][]);
+    }
+
+
+    public static void main(String[] args) {
+
+        // [[1,3],[2,6],[8,10],[15,18]]
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        InsertIntervals insertIntervals = new InsertIntervals();
+        int[][] ints = insertIntervals.insertIntervals(intervals, new int[]{4, 8});
+
+        for (int[] x : ints) {
+            System.out.print("[");
+            for (int y : x) {
+                System.out.print(y);
+            }
+            System.out.println("]");
+        }
+    }
+}
