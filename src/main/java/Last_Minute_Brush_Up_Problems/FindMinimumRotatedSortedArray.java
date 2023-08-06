dae5f2f745e62d5d2a2fdcd126d5cd9d40e3782e
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-06 13:48:43 +0530

    dsa

---
+++ src/main/java/Last_Minute_Brush_Up_Problems/FindMinimumRotatedSortedArray.java
@@ -0,0 +1,43 @@
+package Last_Minute_Brush_Up_Problems;
+
+public class FindMinimumRotatedSortedArray {
+
+
+
+
+    public int findMinimumRotatedSortedArray(int[]arr)
+    {
+
+        int s = 0;
+        int e = arr.length-1;
+
+        while (s<e)
+        {
+
+            int m = (s+e)>>>1;
+
+            if(arr[m]>arr[e])
+            {
+                s=m+1;
+            } else if (arr[m]<arr[e]) {
+
+                e=m;
+            }
+            else {
+                e--;
+            }
+        }
+
+        return arr[s];
+    }
+
+
+    public static void main(String[] args) {
+
+        int[]arr = {4,5,6,7,8,9,1,2,3};
+
+        FindMinimumRotatedSortedArray findMinimumRotatedSortedArray = new FindMinimumRotatedSortedArray();
+
+        System.out.println("Minimum is : "+findMinimumRotatedSortedArray.findMinimumRotatedSortedArray(arr));
+    }
+}
