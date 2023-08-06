34b826d4d041fee32eb795748a5e355cfb1e06a4
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-06 13:04:56 +0530

    dsa

--- src/main/java/Good_Solutions_Problems/NextGreaterElement.java
+++ src/main/java/Good_Solutions_Problems/NextGreaterElement.java
@@ -1,30 +1,31 @@
 package Good_Solutions_Problems;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class NextGreaterElement {
-    public static int[] findNextGreaterElements(int[] nums) {
-        int[] result = new int[nums.length];
-        Arrays.fill(result, -1); // Initialize result array with -1
-
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                int index = stack.pop();
-                result[index] = nums[i];
+    public static int[] findNextGreaterElements(int[] arr) {
+        int[] result = new int[arr.length];
+        Arrays.fill(result, -1); // Initialize with -1
+        
+        Stack<Integer> stack = new Stack<>(); // Store indices
+        
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                result[stack.pop()] = arr[i];
             }
             stack.push(i);
         }
+        
         return result;
     }
-
+    
     public static void main(String[] args) {
-        int[] nums = {4, 5, 2, 25, 10, 8, 6, 12};
-        int[] nextGreaterElements = findNextGreaterElements(nums);
+        int[] arr = {4, 5, 2, 25};
+        int[] nextGreater = findNextGreaterElements(arr);
+        
         System.out.println("Element\tNext Greater Element");
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i] + "\t\t" + nextGreaterElements[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "\t\t" + nextGreater[i]);
         }
     }
 }
