8c840aa6edcb888bfb090bb1fc39284f3f485f76
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-13 01:16:52 +0530

    dsa

--- src/main/java/CSAug2023/Arrays/Section60/SegregateOneZero.java
+++ src/main/java/CSAug2023/Arrays/Section60/SegregateOneZero.java
@@ -26,8 +26,9 @@ public class SegregateOneZero {
             if (left < right) {
 
 
-                arr[left] = 0;
-                arr[right] = 1;
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
                 left++;
                 right--;
             }
@@ -38,5 +39,11 @@ public class SegregateOneZero {
     public static void main(String[] args) {
 
         int[] arr = {1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1};
+        SegregateOneZero segregateOneZero = new SegregateOneZero();
+        segregateOneZero.segregateZeroOnes(arr);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
     }
 }
