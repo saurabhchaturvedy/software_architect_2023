8c6a5d29dfabbfb1a5092458092f4d5d21d01f96
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-13 13:50:35 +0530

    dsa

--- src/main/java/CSAug2023/BinaryTree/Section10/DifferenceEvenOddLevels.java
+++ src/main/java/CSAug2023/BinaryTree/Section10/DifferenceEvenOddLevels.java
@@ -1,5 +1,8 @@
 package CSAug2023.BinaryTree.Section10;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DifferenceEvenOddLevels {
 
 
@@ -13,6 +16,48 @@ public class DifferenceEvenOddLevels {
     }
 
 
+    public int differenceEvenOddLevelsIterative(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int level = 1;
+        int evenSum = 0;
+        int oddSum = 0;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+
+                TreeNode temp = queue.poll();
+
+                if (level % 2 == 0) {
+                    evenSum = evenSum + temp.data;
+                } else {
+                    oddSum = oddSum + temp.data;
+                }
+
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+            }
+
+            level++;
+        }
+
+        return oddSum-evenSum;
+    }
+
+
     public static void main(String[] args) {
 
         TreeNode root = new TreeNode(30);
@@ -22,5 +67,9 @@ public class DifferenceEvenOddLevels {
         DifferenceEvenOddLevels differenceEvenOddLevels = new DifferenceEvenOddLevels();
         int sum = differenceEvenOddLevels.differenceEvenOddLevels(root);
         System.out.println("Difference in levels = " + sum);
+
+
+        int sumIterative = differenceEvenOddLevels.differenceEvenOddLevelsIterative(root);
+        System.out.println("Difference in levels = " + sumIterative);
     }
 }
