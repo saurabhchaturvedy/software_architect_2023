869eeb2608016decbc3b2fa581280b65305b97f6
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-13 13:04:48 +0530

    dsa

---
+++ src/main/java/CSAug2023/BinaryTree/BinaryTree.java
@@ -0,0 +1,13 @@
+package CSAug2023.BinaryTree;
+
+
+public class BinaryTree {
+
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(23);
+        root.left = new TreeNode(37);
+        root.right = new TreeNode(51);
+    }
+}
---
+++ src/main/java/CSAug2023/BinaryTree/TreeNode.java
@@ -0,0 +1,16 @@
+package CSAug2023.BinaryTree;
+
+import Last_Minute_Brush_Up_Problems.ListNode;
+
+public class TreeNode {
+
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
