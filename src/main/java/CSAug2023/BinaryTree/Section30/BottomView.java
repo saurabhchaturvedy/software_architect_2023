3eeb3c38df4e26c4b9d362565830350654466dcb
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-08-13 23:49:49 +0530

    dsa

--- src/main/java/CSAug2023/BinaryTree/Section30/BottomView.java
+++ src/main/java/CSAug2023/BinaryTree/Section30/BottomView.java
@@ -1,29 +1,24 @@
 package CSAug2023.BinaryTree.Section30;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.TreeMap;
+import java.util.*;
 
-public class TopView {
+public class BottomView {
 
 
-    public void topView(TreeNode root) {
+    public void bottomView(TreeNode root) {
         if (root == null) {
             return;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
-        TreeMap<Integer, Integer> topView = new TreeMap<>();
         queue.offer(root);
+        Map<Integer, Integer> bottomViewMap = new TreeMap<>();
 
         while (!queue.isEmpty()) {
-
             TreeNode temp = queue.poll();
             int hd = temp.height;
 
-            if (!topView.containsKey(hd)) {
-                topView.put(hd, temp.data);
-            }
+            bottomViewMap.put(hd, temp.data);
 
             if (temp.left != null) {
                 temp.left.height = hd - 1;
@@ -31,30 +26,23 @@ public class TopView {
             }
 
             if (temp.right != null) {
-                temp.left.height = hd + 1;
+
+                temp.right.height = hd + 1;
                 queue.offer(temp.right);
             }
         }
 
-        System.out.print(topView.values());
+        System.out.print(bottomViewMap.values() + " ");
     }
 
 
     public static void main(String[] args) {
 
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(20);
+        root.right = new TreeNode(30);
 
-        TreeNode root = new TreeNode(15);
-
-        root.left = new TreeNode(38);
-
-        root.right = new TreeNode(44);
-
-        TopView topView = new TopView();
-
-        topView.topView(root);
+        BottomView bottomView = new BottomView();
+        bottomView.bottomView(root);
     }
 }
-
-
-
-
