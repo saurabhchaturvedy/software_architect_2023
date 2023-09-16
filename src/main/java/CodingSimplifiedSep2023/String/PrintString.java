91f84d52a8483a2fd102737143d4a594a9adcec7
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-16 12:45:17 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/String/PrintString.java
@@ -0,0 +1,12 @@
+package CodingSimplifiedSep2023.String;
+
+public class PrintString {
+
+
+    public static void main(String[] args) {
+
+        String str = "Hello world";
+
+        System.out.println(str);
+    }
+}
