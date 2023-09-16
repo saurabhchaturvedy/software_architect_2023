4832c5f7bb50612d4791b77c54adf6d0c57056f1
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-16 13:12:52 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/String/TrimString.java
@@ -0,0 +1,48 @@
+package CodingSimplifiedSep2023.String;
+
+public class TrimString {
+
+
+    public static void main(String[] args) {
+
+
+        String str = "   Hello     word!!!    ";
+
+
+        System.out.println(" String length : " + str.length());
+
+        char[] arr = str.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        for (int i = 0; i < str.length(); i++) {
+            if (arr[i] != ' ') {
+                start = i;
+                break;
+            }
+        }
+
+        for (int i = arr.length - 1; i > start - 1; i--) {
+
+            if (arr[i] != ' ') {
+                end = i;
+                break;
+            }
+        }
+
+        int count = end - start + 1;
+
+        String trimmedStr = new String(arr, start, count);
+
+
+        System.out.println(" String length : " + trimmedStr.length());
+
+        System.out.println(" Trimmed string is : " + trimmedStr);
+
+
+    }
+
+
+}
