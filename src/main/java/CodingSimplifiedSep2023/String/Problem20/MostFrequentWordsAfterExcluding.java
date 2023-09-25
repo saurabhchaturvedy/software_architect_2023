61289e211bc34e51e62c60a23128579ca4ac5057
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-25 20:29:21 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/String/Problem20/MostFrequentWordsAfterExcluding.java
@@ -0,0 +1,66 @@
+package CodingSimplifiedSep2023.String.Problem20;
+
+import java.util.*;
+
+public class MostFrequentWordsAfterExcluding {
+
+
+    public static List<String> mostFrequent(String str, String[] excludingItems) {
+
+        str = str.replaceAll("[^a-zA-Z0-9]", " ");
+        String[] allWords = str.split(" +");
+
+        Map<String, Integer> countingMap = new HashMap<>();
+
+        for (String word : allWords) {
+            word = word.toLowerCase();
+            countingMap.put(word, countingMap.getOrDefault(word, 0) + 1);
+        }
+
+        for (String excluded : excludingItems) {
+            if (countingMap.containsKey(excluded)) {
+                countingMap.remove(excluded);
+            }
+        }
+
+        TreeMap<String, Integer> mostFrequentMap = new TreeMap<>((e1, e2) -> {
+            int freq1 = countingMap.get(e1);
+            int freq2 = countingMap.get(e2);
+
+            if (freq1 != freq2) {
+                return freq2 - freq1;
+            }
+
+            return e1.compareTo(e2);
+        });
+
+        mostFrequentMap.putAll(countingMap);
+
+        List<String> mostFrequentWords = new ArrayList<>();
+
+        int topFreq = mostFrequentMap.firstEntry().getValue();
+
+        while (!mostFrequentMap.isEmpty()) {
+            Map.Entry<String, Integer> word = mostFrequentMap.pollFirstEntry();
+
+            if (word.getValue() == topFreq) {
+                mostFrequentWords.add(word.getKey());
+            } else {
+                break;
+            }
+        }
+
+        return mostFrequentWords;
+    }
+
+    public static void main(String[] args) {
+        String str = "Best items in category are Samsung lenovo. Samsung items are nice. Lenovo are cool";
+
+        String[] excludingItems = {"in", "are"};
+
+        System.out.println(MostFrequentWordsAfterExcluding.mostFrequent(str, excludingItems));
+
+    }
+
+
+}
