baba2929cd38093f06092c7d72fb94cd5d9be54e
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-28 21:11:46 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/Intervals/MergeIntervals.java
@@ -0,0 +1,56 @@
+package CodingSimplifiedSep2023.Intervals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeIntervals {
+
+
+    public int[][] mergeIntervals(int[][] intervals) {
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+
+        List<int[]> intervalList = new ArrayList<>();
+
+        int[] newInterval = intervals[0];
+        intervalList.add(newInterval);
+
+        for (int[] interval : intervals) {
+
+            if (interval[0] <= newInterval[1]) {
+
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            } else {
+
+                newInterval = interval;
+                intervalList.add(newInterval);
+            }
+        }
+
+        return intervalList.toArray(new int[intervalList.size()][]);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+
+        MergeIntervals mergeIntervals = new MergeIntervals();
+
+        int[][] ints = mergeIntervals.mergeIntervals(intervals);
+
+        for (int[] array : ints) {
+
+            System.out.print(" [ ");
+            for (int x : array) {
+
+                System.out.print(x + " ");
+            }
+            System.out.print(" ] ");
+        }
+    }
+}
