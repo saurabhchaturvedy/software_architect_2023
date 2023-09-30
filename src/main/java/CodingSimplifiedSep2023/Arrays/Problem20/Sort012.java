53f66c14e816583913973ee975a0d503301ce49b
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-30 12:34:26 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/Arrays/Problem20/Sort012.java
@@ -0,0 +1,58 @@
+package CodingSimplifiedSep2023.Arrays.Problem20;
+
+public class Sort012 {
+
+
+    public void sort012(int[] arr) {
+
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+
+        }
+    }
+
+    private void swap(int[] arr, int low, int mid) {
+
+
+        int temp = arr[low];
+        arr[low] = arr[mid];
+        arr[mid] = temp;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {0, 1, 2, 2, 2, 0, 0, 0, 2, 1, 1, 1, 2, 2, 0, 0, 1, 2, 0};
+
+        Sort012 sort012 = new Sort012();
+
+        sort012.sort012(arr);
+
+        for (int x : arr) {
+
+            System.out.print(x + " ");
+        }
+    }
+}
