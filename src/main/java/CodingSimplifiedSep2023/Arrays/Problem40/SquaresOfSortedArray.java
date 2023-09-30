fa6d7c1b1227b81c9ab99ce5ed5af4c3f088ecff
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-09-30 22:53:29 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/Arrays/Problem40/SquaresOfSortedArray.java
@@ -0,0 +1,48 @@
+package CodingSimplifiedSep2023.Arrays.Problem40;
+
+public class SquaresOfSortedArray {
+
+
+    public int[] squaresOfSortedArray(int[] arr) {
+
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        for (int k = arr.length - 1; k >= 0; k--) {
+
+            if (arr[left] >= arr[right]) {
+
+                result[k] = arr[left];
+                left++;
+            } else {
+
+                result[k] = arr[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {-6, -3, -1, 2, 4, 5};
+
+        SquaresOfSortedArray squaresOfSortedArray = new SquaresOfSortedArray();
+
+        int[] ints = squaresOfSortedArray.squaresOfSortedArray(arr);
+
+        for (int x : ints) {
+            System.out.print(x + " ");
+        }
+    }
+}
