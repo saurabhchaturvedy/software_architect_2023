3f6654858291a39e4b1b292ec5a7d1af7eb93730
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2023-10-07 14:47:44 +0530

    dsa

---
+++ src/main/java/CodingSimplifiedSep2023/LinkedList/Problem50/SwapGivenTwoNodes.java
@@ -0,0 +1,117 @@
+package CodingSimplifiedSep2023.LinkedList.Problem50;
+
+import CodingSimplifiedSep2023.LinkedList.ListNode;
+
+public class SwapGivenTwoNodes {
+
+
+    public ListNode insert(ListNode node, int data) {
+
+        if (node == null) {
+            return new ListNode(data);
+        } else {
+
+            node.next = insert(node.next, data);
+        }
+
+        return node;
+    }
+
+
+    public void print(ListNode node) {
+
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.data + " ");
+        print(node.next);
+    }
+
+
+    public ListNode swapGivenTwoNodes(ListNode node, int val1, int val2) {
+
+        if (node == null && node.next == null) {
+            return node;
+        }
+
+
+        ListNode prev1 = null;
+        ListNode prev2 = null;
+        ListNode t1 = null;
+        ListNode t2 = null;
+
+
+        ListNode current = node;
+
+        while (current != null) {
+
+            if (current.data == val1) {
+                t1 = current;
+                break;
+            }
+
+            prev1 = current;
+            current = current.next;
+        }
+
+        ListNode current2 = node;
+
+        while (current2 != null) {
+            if (current2.data == val2) {
+                t2 = current2;
+                break;
+            }
+
+            prev2 = current2;
+            current2 = current2.next;
+        }
+
+
+        if (t1 == null || t2 == null) {
+            return node;
+        }
+
+        if (prev1 != null) {
+            prev1.next = t2;
+        } else {
+
+            node = t2;
+        }
+
+        if (prev2 != null) {
+            prev2.next = t1;
+        } else {
+            node = t1;
+        }
+
+        ListNode temp = t1.next;
+        t1.next = t2.next;
+        t2.next = temp;
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+
+        SwapGivenTwoNodes swapGivenTwoNodes = new SwapGivenTwoNodes();
+
+        ListNode root = null;
+
+        root = swapGivenTwoNodes.insert(root, 34);
+        root = swapGivenTwoNodes.insert(root, 52);
+        root = swapGivenTwoNodes.insert(root, 78);
+        root = swapGivenTwoNodes.insert(root, 15);
+        root = swapGivenTwoNodes.insert(root, 63);
+        root = swapGivenTwoNodes.insert(root, 99);
+
+        swapGivenTwoNodes.print(root);
+
+        ListNode listNode = swapGivenTwoNodes.swapGivenTwoNodes(root, 52, 63);
+
+        System.out.println();
+
+        swapGivenTwoNodes.print(listNode);
+    }
+}
